5bc4e381d5a0e0519fbb7384924b515de84edfdb
Joseph Micla <josephmicla@gmail.com>
2021-11-24 20:04:00 -0800

    changes students and teachers to user parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,15 +8,17 @@ const pool = new Pool({
 });
 
 const cohortMonth = process.argv[2];
-const resultLimit = process.argv[3] 
-
-pool.query(`
+const resultLimit = process.argv[3] || 5; 
+const values = [`${cohortMonth}`, resultLimit];
+const queryString = `
 SELECT students.id, students.name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohortMonth}%'
-LIMIT ${resultLimit || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,22 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohortMonth = process.argv[2];
+const cohortMonth = [process.argv[2]];
+const queryString =  `
+  SELECT 
+    teachers.name AS teacher,
+    cohorts.name AS cohort,
+    COUNT(*) AS total_assistances
+  FROM assistance_requests
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  JOIN teachers ON teacher_id = teachers.id
+  WHERE cohorts.name = $1
+  GROUP BY teachers.name, cohorts.name
+  ORDER BY teacher;
+`
 
-pool.query(`
-SELECT 
-  teachers.name AS teacher,
-  cohorts.name AS cohort,
-  COUNT(*) AS total_assistances
-FROM assistance_requests
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-JOIN teachers ON teacher_id = teachers.id
-WHERE cohorts.name = '${cohortMonth}'
-GROUP BY teachers.name, cohorts.name
-ORDER BY teacher;
-
-`)
+pool.query(queryString, cohortMonth)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohort}: ${teacher.teacher}`);
